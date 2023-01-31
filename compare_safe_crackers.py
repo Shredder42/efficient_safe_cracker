2e2d7be6d3f371522d3d094cec96c0f7dd0d60a1
shredder <atbinder42@gmail.com>
2023-01-30 19:45:11 -0600

    generate random combo & get rid of print statements

--- compare_safe_crackers.py
+++ compare_safe_crackers.py
@@ -9,10 +9,6 @@ def fitness(combo, attempt):
             grade += 1
     return grade
 
-def show_digit_panel(solved_wheel):
-    '''Simulates the digital panel solving the combo one number at a time.'''
-    print(solved_wheel)
-
 def original_safe_cracker(lock_combo):
     # generate guess & grade fitness
     best_attempt = [0] * len(combo)
@@ -34,14 +30,9 @@ def original_safe_cracker(lock_combo):
         if next_try_grade > best_attempt_grade:
             best_attempt = next_try[:]
             best_attempt_grade = next_try_grade
-        print(next_try, best_attempt)
         count += 1
 
-    print()
-    print('Cracked! {}'.format(best_attempt), end = ' ')
-    print('in {} tries!'.format(count))
-
-
+    return best_attempt, count
 
 def efficient_safe_cracker(lock_combo):
     # generate guess and grade fitness
@@ -69,33 +60,35 @@ def efficient_safe_cracker(lock_combo):
                 best_attempt_grade = next_try_grade
                 solved_wheel[lock_wheel] = next_try[lock_wheel]
                 locked_wheels.append(lock_wheel)
-                show_digit_panel(solved_wheel)
             elif next_try_grade < best_attempt_grade: # checks if changed wheel FROM correct digit
                 solved_wheel[lock_wheel] = best_attempt[lock_wheel]
                 locked_wheels.append(lock_wheel)
-                show_digit_panel(solved_wheel)
             count += 1
 
+    return best_attempt, count
 
-    print('Cracked! {}'.format(best_attempt), end=' ')
-    print('in {} tries'.format(count))
-
-    return best_attempt
-
-if __name__ == '__main__':
-    '''Use hill climbing method to solve lock combination.'''
-    combination = '6822858902'
-    # Convert combination to list
-    combo = [int(i) for i in combination]
-
+def main():
     original_start_time = time.time()
     original_safe_cracker(combo)
     original_end_time = time.time()
     original_duration = original_end_time - original_start_time
-    print('\nRuntime for original program was {:.5f} seconds'.format(original_duration))
+
     print()
     efficient_start_time = time.time()
     efficient_safe_cracker(combo)
     efficient_end_time = time.time()
     efficient_duration = efficient_end_time - efficient_start_time
-    print('\nRuntime for efficient program was {:.5f} seconds'.format(efficient_duration))
+
+
+
+if __name__ == '__main__':
+    '''Use hill climbing method to solve lock combination.'''
+    # Generate random combination
+    combination = ''
+    for i in range(10):
+        combination += str(randint(0,9))
+    # Convert combination to list
+    combo = [int(i) for i in combination]
+
+    main()
+
